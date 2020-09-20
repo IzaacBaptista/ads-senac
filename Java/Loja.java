c9dded35beb7a6679d197fa6538dd0b53dd62595
Izaac Baptista <izaacbaptista@gmail.com>
2020-09-20 20:25:22 -0300

    Novo exercício em java

---
+++ Java/Loja.java
@@ -0,0 +1,10 @@
+public class Loja {
+
+    public static void main(String[] args) {
+        Cerveja brahma = new Cerveja("Brahma", "Pilsen", 5, 15);
+        Cerveja opaPuroMalte = new Cerveja("Opa", "Puro Malte", 7, 8);
+
+        
+    }
+    
+}
