0e242989d459f8be7cebb2a1449b52b9b1d3f0cc
Izaac Baptista <izaacbaptista@gmail.com>
2020-09-24 21:08:17 -0300

    update exercicios java

--- Java/Banco.java
+++ Java/Banco.java
@@ -1,15 +1,17 @@
     
 public class Banco {
- public static void main(String[] args) {
-     new ContaCorrente(1020, 0014 - 1, 50.00, "Correnteista01");
+
+    public static void main(String[] args) {
+    ContaCorrenteDois new ContaCorrenteDois (1020, 0014 - 1, 50.00, "Correntista01");
  
- System.out.println(ContaCorrente.saldo);
- try{ 
- ContaCorrente.depositar(10);
- } catch (Exception e) {
- System.out.println(e.getMessage());
- }
- }
+    System.out.println(ContaCorrente.saldo);
+    
+    try{ 
+        ContaCorrente.depositar(10);
+    } catch (Exception e) {
+        System.out.println(e.getMessage());
+    }
+    }
 }
 
 
---
+++ Java/ContaCorrenteDois.java
@@ -0,0 +1,27 @@
+
+/*
+Crie uma classe que represente uma Conta Corrente, devendo ter as propriedades Número da Conta, 
+Agência, Saldo, Nome do Titular e os métodos Depósito, Saque e Saldo Total.
+*/
+public class ContaCorrenteDois {
+String numeroConta;
+String agencia;
+double saldo;
+String nomeTitular;
+
+public ContaCorrenteDois(String numeroConta, String agencia, String nomeTitular) {
+    this.numeroConta = numeroConta;
+    this.agencia = agencia;
+    this.nomeTitular = nomeTitular; 
+    this.saldo = 0;
+}
+public void deposito(double valor) {
+    this.saldo += valor;
+}
+public void saque(double valor) {
+    this.saldo -= valor;
+}
+public double saldoTotal() {
+    return saldo;
+}
+}
