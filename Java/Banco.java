9faa776aa956e88a4f10686a0f45667b787fce04
Izaac Baptista <izaacbaptista@gmail.com>
2020-09-25 11:10:36 -0300

    update edit

--- Java/Banco.java
+++ Java/Banco.java
@@ -7,21 +7,21 @@ public class Banco {
         ContaCorrente contaCorrenteUm = new ContaCorrente("04798-5", agenciaUm, "Wanderley da Silva");
         ContaCorrente contaCorrenteDois = new ContaCorrente("05878-4", agenciaUm, "Lidiane Alves");
 
-        System.out.println("O saldo inicial é: R$ " + contaCorrenteUm.saldoTotal());
+        System.out.println("O saldo inicial da ContaUm é: R$ " + contaCorrenteUm.saldoTotal());
 
         contaCorrenteUm.deposito(10000);
         contaCorrenteUm.saque(5485.45);
         contaCorrenteUm.saque(178.97);
 
-        System.out.println("O saldo atual é: R$ " + contaCorrenteUm.saldoTotal());
+        System.out.println("O saldo atual da ContaUm é: R$ " + contaCorrenteUm.saldoTotal());
 
-        System.out.println("O saldo inicial é: R$ " + contaCorrenteDois.saldoTotal());
+        System.out.println("O saldo inicial da ContaDois é: R$ " + contaCorrenteDois.saldoTotal());
 
         contaCorrenteDois.deposito(5000);
         contaCorrenteDois.saque(185.45);
         contaCorrenteDois.saque(578.97);
 
-        System.out.println("O saldo atual é: R$ " + contaCorrenteDois.saldoTotal());
+        System.out.println("O saldo atual da ContaDois é: R$ " + contaCorrenteDois.saldoTotal());
 
         agenciaUm.imprimirContas();
     }
