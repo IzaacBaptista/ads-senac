876337b2b55efdcbb2e143746f412d8e84ac711f
Izaac Baptista <izaacbaptista@gmail.com>
2020-09-25 11:06:39 -0300

    update exec java

--- Java/Banco.java
+++ Java/Banco.java
@@ -1,17 +1,20 @@
-    
+/*
+Crie um programa que receba a conta corrente e deposite e saque valores, ao final imprimindo o saldo existente.
+*/
 public class Banco {
+    public static void main(String[] args) {
+        Agencia agenciaUm = new Agencia("0145", "Rua Jaguariuna","Das 08 as 18");
+        ContaCorrente contaCorrenteUm = new ContaCorrente("04798-5", agenciaUm, "Wanderley da Silva");
+        ContaCorrente contaCorrenteDois = new ContaCorrente("05878-4", agenciaUm, "Lidiane Alves");
 
-    public static void main(final String[] args) {
-        new ContaCorrenteDois(1020, 0014 - 1, 50.00, "Correntista01");
- 
-    System.out.println(ContaCorrente.saldo);
-    
-    try{ 
-        ContaCorrente.depositar(10);
-    } catch (final Exception e) {
-        System.out.println(e.getMessage());
-    }
+        System.out.println("O saldo inicial é: R$ " + contaCorrenteUm.saldoTotal());
+
+        contaCorrenteUm.deposito(10000);
+        contaCorrenteUm.saque(5485.45);
+        contaCorrenteUm.saque(178.97);
+
+        System.out.println("O saldo atual é: R$ " + contaCorrenteUm.saldoTotal());
+
+        agenciaUm.imprimirContas();
     }
 }
-
-
