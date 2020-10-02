9ccb0153865b87832069790abf93062eef7b3363
Izaac Baptista <izaacbaptista@gmail.com>
2020-10-02 09:45:55 -0300

    edit update

--- Java/Locadora.java
+++ Java/Locadora.java
@@ -2,10 +2,10 @@ public class Locadora {
     public static void main(String[] args) {
         Franquia vingadores = new Franquia(1, "Infinity War", "Guerra Infinita", "2018");
         Filme theAvengers = new Filme(1, "2012-2019", 7.99);
-        Heroi homemDeFerro = new Heroi(1, "Homem de Ferro", "New York", "Tony Stark", 4.99, );
+        Heroi homemDeFerro = new Heroi(1, "Homem de Ferro", "New York", "Tony Stark", 4.99, vingadores);
 
-        vingadores.quantidadeDeHerois();
-        theAvengers.quantidadeDeHeroisNoFilme();
+        vingadores.quantidadeDeHerois(homemDeFerro);
+        theAvengers.quantidadeDeHeroisNoFilme(null);
         theAvengers.totalInvestidoComOsHerois();
         homemDeFerro.totalInvestimento();
         homemDeFerro.qntdFilmes();
