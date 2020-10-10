f5a330d49e0c84922e173868fff7a7463ddbeae2
Izaac Baptista <izaacbaptista@gmail.com>
2020-10-09 22:15:10 -0300

    poo

--- Java/Franquia.java
+++ Java/Franquia.java
@@ -1,18 +1,24 @@
 import java.util.ArrayList;
 
 public class Franquia {
-    int id;
+    int idFranquia;
     String nome;
     String apelido;
     String dataDeCriacao;
     ArrayList<Heroi> herois;
 
-    public Franquia(int id, String nome, String apelido, String dataDeCriacao) {
-        this.id = id;
+    public Franquia(int idFranquia, String nome, String apelido, String dataDeCriacao) {
+        this.idFranquia = idFranquia;
         this.nome = nome;
         this.apelido = apelido;
         this.dataDeCriacao = dataDeCriacao;
         this.herois = new ArrayList<>();
+
+        franquias.add(this);
+    }
+    //referenciando (polimorfismo) o construtor FRANQUIA
+    public Franquia (int idFranquia, String nome){
+        this(idFranquia, nome, "", "");
     }
 
     public void quantidadeDeHerois(Heroi heroi) {
