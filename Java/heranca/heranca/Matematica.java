1ddb4219752fb6a81f5a92ab4bddba436f8c0c86
Izaac Baptista <izaacbaptista@gmail.com>
2020-10-30 19:24:09 -0300

    heranca

---
+++ Java/heranca/heranca/Matematica.java
@@ -0,0 +1,20 @@
+package heranca.heranca;
+
+/**
+ * Biblioteca Matemática
+ */
+public class Matematica {
+
+    private Matematica() {
+    }
+
+    // Metodo de Objeto
+    public int somaObj(int x, int y) {
+        return x + y;
+    }
+
+    // Metodo de Classe (static)
+    public static int somaClasse(int x, int y) {
+        return x + y;
+    }
+}
