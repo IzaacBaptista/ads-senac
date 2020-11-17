de0210c4030920d03302318e5a337e1ed0f69e1d
Izaac Baptista <izaacbaptista@gmail.com>
2020-11-17 20:04:09 -0300

    locacao

--- Java/ProvaFinal/Locacao.java
+++ Java/ProvaFinal/Locacao.java
@@ -2,6 +2,9 @@ package ProvaFinal;
 //coments
 import java.util.ArrayList;
 import java.util.Date;
+import java.text.DateFormat;
+import java.util.List;
+import java.util.Calendar;
 
 public class Locacao {
     int idLocacao;
@@ -10,6 +13,10 @@ public class Locacao {
     Date dataDeDevolucao;
     Cliente cliente;
     ArrayList<VeiculosLocados> veiculosLocados;
+    //private Object veiculos;
+
+    List<Veiculo> veiculos = new ArrayList<>();
+	Calendar dataX = Calendar.getInstance();
 
     public Locacao(
         int idLocacao, 
@@ -27,5 +34,39 @@ public class Locacao {
     }
 
 	public void setCliente(Cliente cliente) {
+
+    }
+    
+    // Adição de Veiculos
+	void AdicVeiculo(Veiculo veiculo) {
+		this.veiculos.add(veiculo);
+		//Contador Carros Locado
+		veiculo.getIdVeiculoLocado();
+		this.cliente.qtdVeiculosLocados();
+	}
+
+	double PrecoTotal() {
+		double total = 0;
+		for (Veiculo veiculo : veiculos) {
+			total += veiculo.valorLoc;
+		}
+		return total;
+	}
+
+	// Método com a Quantidade de Veiculos Locados
+	int qtdVeiculosLocados() {
+		return ((ArrayList<VeiculosLocados>) this.veiculosLocados).size();
+	}
+
+	// Calculo Data de Devolução
+	String calculoData() {
+		Date dataAtual = new Date();
+		dataX.setTime(dataAtual);
+		dataX.add(Calendar.DATE, cliente.diaDev);
+
+		// Formatando a Impressão da Data
+		DateFormat dataEdit = DateFormat.getDateInstance(DateFormat.SHORT);
+
+		return dataEdit.format(dataX.getTime());
 	}
 }
